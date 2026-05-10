e88a91c8ef9e0d9d38eb75ec459ca78af70a4817
Arwa <arwa.abderahman@gmail.com>
2026-05-10 12:27:13 +0300

    Adding dark theme

---
+++ theme.js
@@ -0,0 +1,17 @@
+const themeBtn = document.getElementById('theme-toggle');
+const body = document.body;
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+    themeBtn.innerHTML = '<span class="icon"></span> Light Mode';
+}
+themeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+        themeBtn.innerHTML = '<span class="icon"></span> Light Mode';
+    } else {
+        localStorage.setItem('theme', 'light');
+        themeBtn.innerHTML = '<span class="icon"></span> Dark Mode';
+    }
+});
